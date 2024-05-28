169b5d0fded9ab8c606567febc70490335323533
Taras Boychuk <tboychuk@bobocode.com>
2024-05-28 07:24:34 +0300

    Task: Implement a save method of a simple ORM * change JDBC URL

--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -5,7 +5,7 @@ import com.bobocode.entity.Quote;
 
 public class DemoApp {
     public static void main(String[] args) {
-        var orm = new OrmImpl("jdbc:postgresql://0.tcp.eu.ngrok.io:11026/postgres", "bobouser", "bobopass");
+        var orm = new OrmImpl("jdbc:postgresql://0.tcp.eu.ngrok.io:13243/postgres", "bobouser", "bobopass");
 
         var quote = orm.findById(Quote.class, 1);
         System.out.println(quote);
