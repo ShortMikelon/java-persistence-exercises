8cf5dcea8dbcd6d1ea9e33237443581b0dcd70e5
Andrii.Kovalchuk <shtramak@gmail.com>
2024-06-11 19:01:34 +0300

    use multiline SQL string

--- 2-0-jdbc-api/2-0-2-create-table/src/main/java/com/bobocode/AccountDbInitializer.java
+++ 2-0-jdbc-api/2-0-2-create-table/src/main/java/com/bobocode/AccountDbInitializer.java
@@ -32,18 +32,20 @@ public class AccountDbInitializer {
     public void init() throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
             Statement statement = connection.createStatement();
-            statement.execute("CREATE TABLE account(" +
-                    "id BIGINT," +
-                    " email VARCHAR(255) NOT NULL," +
-                    " first_name VARCHAR(255) NOT NULL," +
-                    " last_name VARCHAR(255) NOT NULL," +
-                    " gender VARCHAR(255) NOT NULL," +
-                    " birthday DATE NOT NULL," +
-                    " balance DECIMAL(19,4)," +
-                    " creation_time TIMESTAMP NOT NULL DEFAULT now()," +
-                    " CONSTRAINT account_pk PRIMARY KEY (id)," +
-                    " CONSTRAINT account_email_uq UNIQUE (email)" +
-                    ");");
+            statement.execute("""
+                    CREATE TABLE account(
+                    id BIGINT,
+                    email VARCHAR(255) NOT NULL,
+                    first_name VARCHAR(255) NOT NULL,
+                    last_name VARCHAR(255) NOT NULL,
+                    gender VARCHAR(255) NOT NULL,
+                    birthday DATE NOT NULL,
+                    balance DECIMAL(19,4),
+                    creation_time TIMESTAMP NOT NULL DEFAULT now(),
+                    CONSTRAINT account_pk PRIMARY KEY (id),
+                    CONSTRAINT account_email_uq UNIQUE (email)
+                    );
+                    """);
         }
     }
 }
